a4c860aa2c5d3e4ed45028353cf4331a98d79a6f
endourdev <contact@endour.tech>
2024-07-13 02:39:10 +0200

    Added a lot of debug for DiscordRPC

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// We import all modules here.
 const { app, BrowserWindow } = require("electron");
 const RPC = require("discord-rpc");
 const config = require("./configs/config.json");
@@ -6,8 +5,7 @@ const rpc = new RPC.Client({
     transport: "ipc"
 });
 
-// Main function. He define title and others stuff.
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     title: "YouTube Browser",
     autoHideMenuBar: true,
@@ -16,22 +14,49 @@ function createWindow () {
   });
 
   win.loadURL("https://youtube.com/");
+}
 
-};
-
-// We create main window here.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    };
+      createWindow();
+    }
   });
 });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
-  };
+    app.quit();
+  }
 });
+
+rpc.on('ready', () => {
+  console.log('Discord RPC connected');
+  rpc.setActivity({
+    details: 'Browsing YouTube',
+    startTimestamp: new Date(),
+    largeImageKey: 'youtube_icon',
+    largeImageText: 'YouTube',
+    smallImageKey: 'electron_icon',
+    smallImageText: 'Electron App'
+  });
+});
+
+rpc.on('error', (error) => {
+  console.error('Discord RPC error:', error);
+  console.log('Error details:', error.message);
+  console.log('Stack trace:', error.stack);
+});
+
+console.log('Attempting to connect to Discord with client ID:', config.client);
+rpc.login({ clientId: "1261476866586447922" })
+  .then(() => {
+    console.log('Successfully connected to Discord');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to Discord:', error);
+    console.log('Error details:', error.message);
+    console.log('Stack trace:', error.stack);
+  });
